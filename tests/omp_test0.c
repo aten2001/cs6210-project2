3fec4ca530bc8619b1be2147431c0e971ab8d874
Dilan Manatunga <manatunga@gmail.com>
2016-03-07 23:19:07 -0500

    Updated test printing code

--- tests/omp_test0.c
+++ tests/omp_test0.c
@@ -8,16 +8,18 @@ void centralized_test(int num_threads, int num_iters) {
   omp_centralized_barrier_t barrier;
   omp_centralized_barrier_init(&barrier, num_threads);
 
+  printf("=====Test Start: Centralized Barrier 1 Threads: %d Iters: %d=====\n", num_threads, num_iters);
 #pragma omp parallel
   {
     int N = omp_get_num_threads();
     int thread_num = omp_get_thread_num();
-    for (int i = 0; i < num_iters; i++) {
+    for (int i = 1; i <= num_iters; i++) {
       printf("Process %d/%d: Iteration %d\n", thread_num, N, i);
       omp_centralized_barrier(&barrier);
     }
   }
 
+  printf("=====Test End: Centralized Barrier 1====\n");
   omp_centralized_barrier_destroy(&barrier);
 }
 
@@ -27,15 +29,17 @@ void centralized2_test(int num_threads, int num_iters) {
   omp_centralized_barrier2_t barrier;
   omp_centralized_barrier2_init(&barrier, num_threads);
 
+  printf("=====Test Start: Centralized Barrier 2 Threads: %d Iters: %d=====\n", num_threads, num_iters);
 #pragma omp parallel
   {
     int N = omp_get_num_threads();
     int thread_num = omp_get_thread_num();
-    for (int i = 0; i < num_iters; i++) {
+    for (int i = 1; i <= num_iters; i++) {
       printf("Process %d/%d: Iteration %d\n", thread_num, N, i);
       omp_centralized_barrier2(&barrier);
     }
   }
+  printf("=====Test End: Centralized Barrier 2====\n");
 
   omp_centralized_barrier2_destroy(&barrier);
 }
@@ -46,15 +50,17 @@ void tree_test(int num_threads, int num_iters) {
   omp_tree_barrier_t barrier;
   omp_tree_barrier_init(&barrier, num_threads);
 
+  printf("=====Test Start: Tree Barrier Threads: %d Iters: %d=====\n", num_threads, num_iters);
 #pragma omp parallel
   {
     int N = omp_get_num_threads();
     int thread_num = omp_get_thread_num();
-    for (int i = 0; i < num_iters; i++) {
+    for (int i = 1; i <= num_iters; i++) {
       printf("Process %d/%d: Iteration %d\n", thread_num, N, i);
       omp_tree_barrier(&barrier);
     }
   }
+  printf("=====Test End: Tree Barrier====\n");
 
   omp_tree_barrier_destroy(&barrier);
 }
@@ -65,15 +71,17 @@ void tournament_test(int num_threads, int num_iters) {
   omp_tournament_barrier_t barrier;
   omp_tournament_barrier_init(&barrier, num_threads);
 
+  printf("=====Test Start: Tournament Barrier Threads: %d Iters: %d=====\n", num_threads, num_iters);
 #pragma omp parallel
   {
     int N = omp_get_num_threads();
     int thread_num = omp_get_thread_num();
-    for (int i = 0; i < num_iters; i++) {
+    for (int i = 1; i <= num_iters; i++) {
       printf("Process %d/%d: Iteration %d\n", thread_num, N, i);
       omp_tournament_barrier(&barrier);
     }
   }
+  printf("=====Test End: Tournament Barrier====\n");
 
   omp_tournament_barrier_destroy(&barrier);
 }
@@ -84,15 +92,17 @@ void dissemination_test(int num_threads, int num_iters) {
   omp_dissemination_barrier_t barrier;
   omp_dissemination_barrier_init(&barrier, num_threads);
 
+  printf("=====Test Start: Dissemination Barrier Threads: %d Iters: %d=====\n", num_threads, num_iters);
 #pragma omp parallel
   {
     int N = omp_get_num_threads();
     int thread_num = omp_get_thread_num();
-    for (int i = 0; i < num_iters; i++) {
+    for (int i = 1; i <= num_iters; i++) {
       printf("Process %d/%d: Iteration %d\n", thread_num, N, i);
       omp_dissemination_barrier(&barrier);
     }
   }
+  printf("=====Test End: Dissemination Barrier====\n");
 
   omp_dissemination_barrier_destroy(&barrier);
 }
@@ -103,15 +113,17 @@ void mcs_test(int num_threads, int num_iters) {
   omp_mcs_barrier_t barrier;
   omp_mcs_barrier_init(&barrier, num_threads);
 
+  printf("=====Test Start: MCS Barrier Threads: %d Iters: %d=====\n", num_threads, num_iters);
 #pragma omp parallel
   {
     int N = omp_get_num_threads();
     int thread_num = omp_get_thread_num();
-    for (int i = 0; i < num_iters; i++) {
+    for (int i = 1; i <= num_iters; i++) {
       printf("Process %d/%d: Iteration %d\n", thread_num, N, i);
       omp_mcs_barrier(&barrier);
     }
   }
+  printf("=====Test End: MCS Barrier====\n");
 
   omp_mcs_barrier_destroy(&barrier);
 }
@@ -119,16 +131,17 @@ void mcs_test(int num_threads, int num_iters) {
 void omp_test(int num_threads, int num_iters) {
   omp_set_num_threads(num_threads);
 
+  printf("=====Test Start: OMP Barrier Threads: %d Iters: %d=====\n", num_threads, num_iters);
 #pragma omp parallel
   {
     int N = omp_get_num_threads();
     int thread_num = omp_get_thread_num();
-    for (int i = 0; i < num_iters; i++) {
+    for (int i = 1; i <= num_iters; i++) {
       printf("Process %d/%d: Iteration %d\n", thread_num, N, i);
 #pragma omp barrier
     }
   }
-
+  printf("=====Test End: OMP Barrier====\n");
 }
 
 int main(int argc, char** argv) {
